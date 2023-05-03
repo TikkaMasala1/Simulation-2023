e2390792056daa21879d49fc174c5ed5bb90eb8d
Taran Singh <74255354+TikkaMasala1@users.noreply.github.com>
2023-05-03 15:26:48 +0200

    Add option to set slow_vehicle_max_speed. Added function to calculate the average speed of all the vehicles. Added the option to add events to the simulation

--- 3-Touring-Machines-and-Electric-elections/traffic_model.py
+++ 3-Touring-Machines-and-Electric-elections/traffic_model.py
@@ -1,3 +1,4 @@
+import random
 from collections import defaultdict
 from mesa import Model, Agent
 from mesa.space import SingleGrid
@@ -13,7 +14,7 @@ class Vehicle(Agent):
 
     # Define the step function for the Vehicle agent
     def step(self):
-        # Accelerate if the vehicle's speed is less than its maximum speed, increase its speed by 1
+        # Accelerate If the vehicle's speed is less than its maximum speed, increase its speed by 1
         if self.speed < self.max_speed:
             self.speed += 1
 
@@ -88,13 +89,18 @@ class TrafficModel(Model):
         self.grid = SingleGrid(road_length, 1, torus=True)
         self.schedule = RandomActivation(self)  # Schedule for updating agents in random order
         self.slowdown_probability = slowdown_probability  # Probability of vehicles slowing down
+        self.events = defaultdict(list)
 
         # Create a list of available positions
         available_positions = list(range(road_length))
         self.random.shuffle(available_positions)
 
+        # Set the slow vehicle's maximum speed to half of the regular vehicle's max speed if it's not provided
+        if slow_vehicle_max_speed is None:
+            slow_vehicle_max_speed = max_speed // 2
+
         # Create normal vehicles
-        for i in range(vehicle_count):
+        for i in range(vehicle_count - slow_vehicle_count):
             vehicle = Vehicle(i, self, max_speed)
             position = available_positions.pop()
             self.grid.place_agent(vehicle, (position, 0))
@@ -107,6 +113,33 @@ class TrafficModel(Model):
             self.grid.place_agent(slow_vehicle, (position, 0))
             self.schedule.add(slow_vehicle)
 
-    # Define the step function for the TrafficModel
+        self.step_count = 0
+
+    def calculate_average_speed(self):
+        # Calculate the average speed of all vehicles in the simulation. This method computes the sum of speeds of
+        # all agents (vehicles) in the schedule and divides it by the total number of agents. The result is the
+        # average speed
+        total_speed = sum([agent.speed for agent in self.schedule.agents])
+        average_speed = total_speed / len(self.schedule.agents)
+        return average_speed
+
+    def add_event(self, step, event, repeat_every=None, *args, **kwargs):
+        # Add an event to the model at the specified step or every certain number of steps
+        # The event is stored as a tuple containing the event function, its arguments, and keyword arguments
+        self.events[step].append((event, args, kwargs, repeat_every))
+
     def step(self):
-        self.schedule.step()  # Update all agents in the model
+        # Execute the scheduled agents' steps
+        self.schedule.step()
+
+        # Execute events for the current step
+        # Iterate through the list of events for the current step count
+        for event, args, kwargs, repeat_every in self.events[self.step_count]:
+            event(*args, **kwargs)
+            # Call the event function with its arguments and keyword arguments
+            if repeat_every is not None:
+                next_step = self.step_count + repeat_every
+                self.add_event(next_step, event, repeat_every, *args, **kwargs)
+
+        # Increment the step counter
+        self.step_count += 1
